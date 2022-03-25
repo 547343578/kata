b511395b827490968b60fba6bfc4652662180e5e
Zhuquing Wang <zhuquing@mercadona.es>
2022-03-25 16:56:25 +0100

    created Bowling kata

--- Bowling/src/main/java/com/lucas/Bowling/Game.java
+++ Bowling/src/main/java/com/lucas/Bowling/Game.java
@@ -1,4 +1,68 @@
 package com.lucas.Bowling;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
+
+    private final List<Integer> pinsKnockedDown = new ArrayList<>();
+
+    public void roll(Integer num){
+        pinsKnockedDown.add(num);
+    }
+
+    public Integer score(){
+        int totalPoints = 0;
+        int rolls = 0;
+        for(int frame = 0; frame < 10; frame++){
+            if(isNotIndexOut(rolls)) {
+                if (isStrike(rolls)) {
+                    totalPoints += getStrikePoints(rolls);
+                } else if (isSpare(rolls)) {
+                    totalPoints += getSparePoints(rolls);
+                } else {
+                    totalPoints += getPointsOfthisFrame(rolls);
+                }
+                rolls += 2;
+            }
+        }
+        return totalPoints;
+    }
+
+    private Integer getPointsOfthisFrame(Integer roll){
+        if (isNotIndexOut(roll + 1)){
+            return pinsKnockedDown.get(roll) + pinsKnockedDown.get(roll + 1);
+        }
+        return pinsKnockedDown.get(roll);
+    }
+
+    private Integer getSparePoints(Integer roll){
+        return 10 + pinsKnockedDown.get(roll + 2);
+    }
+
+    private Integer getStrikePoints(Integer roll){
+        Integer points = 10;
+        if (isNotIndexOut(roll + 1)){
+            points += pinsKnockedDown.get(roll + 1);
+        }
+        if (isNotIndexOut(roll + 2)){
+            points += pinsKnockedDown.get(roll + 2);
+        }
+        return points;
+    }
+
+    private Boolean isSpare(Integer roll){
+        if(isNotIndexOut(roll + 1)){
+            return (pinsKnockedDown.get(roll) + pinsKnockedDown.get(roll + 1)) == 10;
+        }
+        return false;
+    }
+
+    private Boolean isStrike(Integer roll){
+        return pinsKnockedDown.get(roll) == 10;
+    }
+
+    private Boolean isNotIndexOut(Integer rolls){
+        return rolls < pinsKnockedDown.size();
+    }
 }
---
+++ Bowling/src/test/java/BowlingTests.java
@@ -0,0 +1,59 @@
+import com.lucas.Bowling.Game;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BowlingTests {
+
+    private Game bowlingGame;
+
+    @Before
+    public void create_bowling_game(){
+        this.bowlingGame = new Game();
+    }
+
+    @Test
+    public void score_0_point(){
+        for(int roll = 0; roll < 20; roll++){
+            bowlingGame.roll(0);
+        }
+        Integer result = bowlingGame.score();
+
+        assertEquals(0,result);
+    }
+
+    @Test
+    public void score_points_but_no_spare_no_strike(){
+        for(int roll = 0; roll < 20; roll++){
+            bowlingGame.roll(2);
+        }
+        Integer result = bowlingGame.score();
+
+        assertEquals(40, result);
+    }
+
+    @Test
+    public void score_points_with_spare(){
+        bowlingGame.roll(5);
+        bowlingGame.roll(5);
+        for(int roll = 0; roll < 18; roll++){
+            bowlingGame.roll(2);
+        }
+        Integer result = bowlingGame.score();
+
+        assertEquals(48, result);
+    }
+
+    @Test
+    public void score_points_with_strike(){
+        bowlingGame.roll(10);
+        for(int roll = 0; roll < 19; roll++){
+            bowlingGame.roll(2);
+        }
+        Integer result = bowlingGame.score();
+
+        assertEquals(50, result);
+    }
+
+}
